da599f5745e41a9355eccc4e5d57150eb35cb53e
KURURU <i@okako.com>
2018-09-18 04:39:25 +0900

    add

---
+++ sketch_sep11a.ino
@@ -0,0 +1,13 @@
+#include <LiquidCrystal.h>
+ 
+LiquidCrystal lcd = LiquidCrystal(12,11,10,5,4,3,2);
+ 
+void setup(){
+lcd.begin(16, 2);
+lcd.print("Hello Arduino");
+lcd.setCursor(0,1);
+lcd.print("Hello Minoru!");
+}
+ 
+void loop(){
+}
