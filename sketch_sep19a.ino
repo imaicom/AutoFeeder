8a72e5a3d4124aa20678a0eccdc3e5df5477d1da
KURURU <i@okako.com>
2018-09-20 16:29:15 +0900

    1
    1

--- sketch_sep19a.ino
+++ sketch_sep19a.ino
@@ -1,6 +1,6 @@
 #include <LiquidCrystal.h>
  
-LiquidCrystal lcd = LiquidCrystal(12,11,10,5,4,3,2);
+LiquidCrystal lcd = LiquidCrystal(12,11,10,5,4,3,1);
 
 int i=0;
 
@@ -9,12 +9,12 @@ pinMode( A0,INPUT_PULLUP ); // 入力プルアップ抵抗付加
 pinMode( A1,INPUT_PULLUP ); // 入力プルアップ抵抗付加
 pinMode( A2,INPUT_PULLUP ); // 入力プルアップ抵抗付加  
 pinMode( A3,INPUT_PULLUP ); // 入力プルアップ抵抗付加  
-pinMode( A4,INPUT_PULLUP ); // 入力プルアップ抵抗付加  
+pinMode(  2,INPUT_PULLUP ); // 入力プルアップ抵抗付加  
 pinMode( A5,INPUT_PULLUP ); // 入力プルアップ抵抗付加  
 lcd.begin(16, 2);
 lcd.print("Hello Arduino");
 lcd.setCursor(0,1);
-lcd.print("Hello Makoto!");
+lcd.print("             ");
 }
  
 void loop(){
@@ -27,9 +27,9 @@ void loop(){
     sprintf(buf,"%04d",i);
     lcd.print(buf);
   };
-  if(!digitalRead(A4)){
-    delay(1);while(!digitalRead(A4));
-    i++;
+  if(!digitalRead(2)){
+    delay(1);while(!digitalRead(2));
+    if(!digitalRead(A5)) {i--;}else{i++;};
     lcd.setCursor(0,1);
     sprintf(buf,"%04d",i);
     lcd.print(buf);
