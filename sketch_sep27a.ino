8a8a4750c10342c3401fb0ead996ca7920c586c4
KURURU <i@okako.com>
2018-09-28 14:47:50 +0900

    1
    1

--- sketch_sep27a.ino
+++ sketch_sep27a.ino
@@ -2,8 +2,11 @@
  
 LiquidCrystal lcd = LiquidCrystal(12,11,10,5,4,3,1);
 
+int m=0;
+int k=2;
 int i=0;
 int j=0;
+int p5=0;
 int z,zz;
 int old_i=0;
 float za;
@@ -25,35 +28,133 @@ digitalWrite( 7, 0 );
 digitalWrite( 8, 0 );
 digitalWrite( 9, 0);
 
-lcd.begin(16, 2);
-lcd.print(" Fwd    Back");
-lcd.setCursor(0,1);
-lcd.print(" Feed   +0.5");
-//lcd.cursor();
-lcd.blink();
 attachInterrupt(0, counta, FALLING);  //INT0 - D2 pin
 }
 
 void loop(){
-  lcd.setCursor(0,0);
-  delay(1000);
-
-  lcd.setCursor(7,0);
-  delay(1000);
-
-  lcd.setCursor(0,1);
-  delay(1000);
   
-  lcd.setCursor(7,1);
-  delay(1000);
+  if(!digitalRead(A2)) {delay(10);while(!digitalRead(A2));m=0;k=2;}
+  
+  if(m==0) {
+    lcd.begin(16, 2);
+    lcd.print(" Fwd    Back");
+    lcd.setCursor(0,1);
+    lcd.print(" Feed   +0.5");
+    lcd.blink();
+    m=1;
+  };  
+  
+  if(m==1) {
+    if(k==2)lcd.setCursor(0,0);
+    if(k==3)lcd.setCursor(7,0);
+    if(k==4)lcd.setCursor(0,1);  
+    if(k==5)lcd.setCursor(7,1);
+    if(!digitalRead(A3)) {delay(10);while(!digitalRead(A3));m=k;}
+  };
+  
+  if(m==2) {
+    delay(10);
+    lcd.begin(16, 2);
+    lcd.print(" Fwd        ");
+    lcd.setCursor(0,1);
+    lcd.print("            ");
+    m=22;
+  };
+  
+  if(m==22) {
+    delay(10);
+    lcd.begin(16, 2);
+    lcd.print(" Fwd        ");
+    lcd.setCursor(0,1);
+    lcd.print("            ");
+    do {
+      
+    } while(digitalRead(A2)); // menu
+    m=0;
+  };
+
+  if(m==3) {
+    delay(10);
+    lcd.begin(16, 2);
+    lcd.print(" Back       ");
+    lcd.setCursor(0,1);
+    lcd.print("            ");
+    m=33;
+  };
+  
+  if(m==33) {
+    delay(10);
+    lcd.begin(16, 2);
+    lcd.print(" Back       ");
+    lcd.setCursor(0,1);
+    lcd.print("            ");
+    do {
+      
+    } while(digitalRead(A2)); // menu
+    m=0;
+  };
+  
+  if(m==4) {
+    delay(10);
+    lcd.begin(16, 2);
+    lcd.print(" Feed       ");
+    lcd.setCursor(0,1);
+    lcd.print("            ");
+    m=44;
+  };
+  
+  if(m==44) {
+    delay(10);
+    lcd.begin(16, 2);
+    lcd.print(" Feed       ");
+    lcd.setCursor(0,1);
+    lcd.print("            ");
+    do {
+      
+    } while(digitalRead(A2)); // menu
+    m=0;
+  };
+
+  if(m==5) {
+    lcd.begin(16, 2);
+    lcd.print(" +0.5       ");
+    lcd.setCursor(0,1);
+    lcd.print("            ");
+    m=55;
+  };
+  
+  if(m==55) {
+    delay(10);
+    lcd.begin(16, 2);
+    lcd.print(" +0.5       ");
+    lcd.setCursor(1,1);
+    lcd.print(p5);
+    do {
+      if(!digitalRead(A3)) {delay(10);while(!digitalRead(A3));
+        p5+=5;if(p5>5)p5=0;
+        lcd.setCursor(1,1);lcd.print(p5);
+      };
+    } while(digitalRead(A2)); // menu
+    m=0;
+  };
+  
 }
 
 void counta() {
 
-  delay(1);
-  if(!digitalRead(A5)) {i=i+(30-j);}else{i=i-(30-j);};
-  if(i<-1400)i=-1400;
-  if(i>1400)i=1400;
-  j=0;
+  if(m==1) {
+    delay(1);
+    if(!digitalRead(A5)) {k++;}else{k--;};
+    if(k<2)k=5;
+    if(k>5)k=2;
+  };
 
+  if(m==44) {
+    delay(1);
+    if(!digitalRead(A5)) {i=i+(30-j);}else{i=i-(30-j);};
+    if(i<-1400)i=-1400;
+    if(i>1400)i=1400;
+    j=0;
+  };
+  
 } //counta()
